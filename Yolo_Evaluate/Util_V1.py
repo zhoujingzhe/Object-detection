7c150bff3c6b2cf6005873a95054ffa6fef078fb
Alexander <zjz201392191@outlook.com>
2018-11-14 12:22:27 +0800

    Merging evaluated util and train util

--- Yolo_Evaluate/Util_V1.py
+++ Yolo_Evaluate/Util_V1.py
@@ -118,53 +118,11 @@ def return_coordinates(y_pred):
     hpred = K.clip(x=hpred, max_value=480, min_value=50)
     return [xpred, ypred, wpred, hpred]
 
-def Loss_v1(y_true, y_pred):
-    Pc_pred = y_pred[:, :, :, 0]
-    Pc_pred = K.sigmoid(x=Pc_pred)
-    Pc_pred = K.cast(Pc_pred, 'float32')
-    xpred, ypred, wpred, hpred = return_coordinates(y_pred)
-    C_Class_Array = y_pred[:, :, :, 5:]
-    C_index_pred = K.argmax(C_Class_Array, axis=-1)
-    x_max_true = y_true[:, :, :, 0]
-    x_min_true = y_true[:, :, :, 1]
-    y_max_true = y_true[:, :, :, 2]
-    y_min_true = y_true[:, :, :, 3]
-    C_index_true = y_true[:, :, :, 6]
-    C_index_true = K.cast(C_index_true, dtype='int64')
-
-    x1_pred, y1_pred, x2_pred, y2_pred = transform_to_coordinate(xpred, ypred, wpred, hpred)
-
-    X_matrix = np.ndarray((19, 14, 2), dtype='float32')
-
-    X_matrix[:, :, 0] = np.arange(0, 608, 32).reshape(19, 1)
-    X_matrix[:, :, 1] = np.arange(0, 417, 32).reshape(1, 14)
-
-    x1_window = X_matrix[:, :, 0]
-    y1_window = X_matrix[:, :, 1]
-    x2_window = X_matrix[:, :, 0] + 64
-    y2_window = X_matrix[:, :, 1] + 64
-
-    matching = Checking_if_object(x1_window, y1_window, x2_window, y2_window, x_max_true, x_min_true,
-                                  y_max_true, y_min_true)
-    mat = K.cast(matching, 'float32')
-
-    global Constant_Classification
-    Classification_loss = weight_Classification_loss * mat * Constant_Classification * K.cast(K.equal(C_index_pred, C_index_true), 'float32')
-
-    Localization_loss = weight_Localization_loss * mat * (K.square(x1_pred - x_min_true) + K.square(x2_pred - x_max_true) + K.square(
-        y1_pred - y_min_true) + K.square(y2_pred - y_max_true))
-
-    Object_loss = weight_Object_loss * mat * K.square(1 - Pc_pred) + (1 - mat) * K.square(Pc_pred)
-
-    Total_loss = K.mean(axis=-1, x=Classification_loss) + K.mean(axis=-1, x=Localization_loss) + K.mean(axis=-1, x=Object_loss)
-    Totalloss = K.mean(x=Total_loss, axis=-1)
-
-    return Totalloss
-
-
 def Loss_v2(y_true, y_pred):
     Pc_pred = y_pred[:, :, :, 0]
     Pc_pred = K.sigmoid(x=Pc_pred)
+    global _epsilon
+    Pc_pred = tf.clip_by_value(t=Pc_pred, clip_value_min = _epsilon, clip_value_max = 1 - _epsilon)
     Pc_pred = K.cast(Pc_pred, 'float32')
     xpred, ypred, wpred, hpred = return_coordinates(y_pred)
     x1_pred, y1_pred, x2_pred, y2_pred = transform_to_coordinate(xpred, ypred, wpred, hpred)
@@ -191,7 +149,6 @@ def Loss_v2(y_true, y_pred):
                                   y_max_true, y_min_true)
     mat = K.cast(matching, 'float32')
 
-    global _epsilon
 
     C_Class_Array = softmax(x = C_Class_Array, axis= -1)
     C_Class_Array = tf.clip_by_value(t=C_Class_Array, clip_value_min = _epsilon, clip_value_max = 1 - _epsilon)
@@ -203,8 +160,8 @@ def Loss_v2(y_true, y_pred):
 
     Localization_loss = weight_Localization_loss * mat * (K.square(x1_pred - x_min_true) + K.square(x2_pred - x_max_true) + K.square(
         y1_pred - y_min_true) + K.square(y2_pred - y_max_true))
-
-    Object_loss = weight_Object_loss * (mat * K.square(1 - Pc_pred) + (1 - mat) * K.square(Pc_pred))
+    Object_loss = -(1 - Pc_pred) * K.log(Pc_pred) * mat - (1 - mat) * K.log(1-Pc_pred) * Pc_pred
+    Object_loss = Object_loss * weight_Object_loss
 
     Total_loss = K.mean(axis=-1, x= K.mean(axis=-1, x=Classification_loss)) + K.mean(axis=-1, x=Localization_loss) + K.mean(axis=-1, x=Object_loss)
     Totalloss = K.mean(x=Total_loss, axis=-1)
