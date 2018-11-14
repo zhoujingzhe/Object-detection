c0ae743de1bdd3fc95ef9bb79fc4713be4de09c0
Alexander <zjz201392191@outlook.com>
2018-11-14 14:09:56 +0800

    lastest version

--- Util_V2.py
+++ Util_V2.py
@@ -1,39 +1,34 @@
-import keras
 import keras.backend as K
-from keras.activations import softmax
 from keras.losses import categorical_crossentropy
+import keras
 import numpy as np
 import tensorflow as tf
 #hyperparameter
-
 from settings import setting
 weight_Classification_loss = setting["weight_Classification_loss"]
 weight_Object_loss = setting["weight_Object_loss"]
 weight_Localization_loss = setting["weight_Localization_loss"]
 _batch_size = setting['batch_size']
 _epoch = 0
-Constant_Classification = 10
-#####
 initial_lr = 0.01
-# Hdecay = 0.01
 Hdecay = setting["decay"]
 lr_minimum_rate = 60.0
-#####
 
-print("--------------------------->")
+#########################
 print(weight_Classification_loss)
 print(weight_Object_loss)
 print(weight_Localization_loss)
 print(Hdecay)
-print("<---------------------------")
-
+print(_batch_size)
+#########################
 _epsilon = K.epsilon()
 _epsilon = K.cast(_epsilon, 'float32')
 
 def generating_consequences(results):
+    global _batch_size
+    _batch_size = K.shape(results)[0]
     Pc = results[:, :, :, 0]
     Pc = K.reshape(x = Pc, shape=(-1, 19, 14, 1))
-    Pc = K.sigmoid(Pc)
     x ,y ,w ,h = return_coordinates(y_pred=results)
     x = K.reshape(x=x, shape=(-1, 19, 14, 1))
     y = K.reshape(x=y, shape=(-1, 19, 14, 1))
@@ -41,7 +36,6 @@ def generating_consequences(results):
     h = K.reshape(x=h, shape=(-1, 19, 14, 1))
     Boxes = K.concatenate([x, y, w, h], axis=-1)
     Class = results[:, :, :, 5:]
-    Class = K.softmax(Class)
     Box_scores = Pc * Class
     Box_classes = K.argmax(Box_scores, axis=-1)
     Box_class_scores = K.max(Box_scores, axis=-1)
@@ -50,42 +44,15 @@ def generating_consequences(results):
     Boxes = K.reshape(x=Boxes, shape=(_batch_size, -1, 4))
     TOPK = tf.nn.top_k(input=Box_class_scores, k=5)
     indices = TOPK.indices
-    temp = np.zeros(indices.shape)
-    temp[:] = np.arange(0, _batch_size, 1).reshape(_batch_size, -1)
+    temp = K.zeros(K.shape(x=indices), dtype='int32')
+    tmp = K.arange(0, _batch_size, 1, dtype='int32')
+    tmp = K.reshape(x=tmp, shape=(_batch_size, -1))
+    temp = temp + tmp
     indices = tf.stack([temp, indices], axis= 2)
-    indices = tf.cast(indices, 'int64')
     scores = tf.gather_nd(params=Box_class_scores, indices=indices)
     boxes = tf.gather_nd(params=Boxes, indices=indices)
     classes = tf.gather_nd(params=Box_classes, indices=indices)
     return boxes, classes, scores
-
-class DecayByEpoch(keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, log=[]):
-        global Hdecay
-        new_lr = initial_lr * 1.0 / (1.0 + Hdecay * epoch)
-        if initial_lr / new_lr > lr_minimum_rate:
-            lr = self.model.optimizer.lr
-        else:
-            K.set_value(self.model.optimizer.lr, new_lr)
-            lr = self.model.optimizer.lr
-        print(K.eval(lr))
-
-class lr_minimum(keras.callbacks.Callback):
-    def on_batch_end(self, batch, log=[]):
-        global Hdecay, _epoch, _batch_size
-        iterations = batch + _epoch * 1000.0 / _batch_size
-        print('iterations:', iterations)
-        new_lr = initial_lr * 1.0 / (1.0 + Hdecay * iterations)
-        print('The New_lr:', new_lr)
-        if initial_lr / new_lr > lr_minimum_rate:
-            K.set_value(self.model.optimizer.lr, initial_lr/lr_minimum_rate)
-        else:
-            K.set_value(self.model.optimizer.lr, new_lr)
-    def on_epoch_end(self, epoch, log=[]):
-        lr = self.model.optimizer.lr
-        global _epoch
-        _epoch += 1
-        print('Each epoch, the lr is', K.eval(lr))
 
 def transform_to_coordinate(x, y, w, h):
     x1 = x - K.cast(w / 2, 'float32')
@@ -104,26 +71,25 @@ def Checking_if_object(x1_window, y1_window, x2_window, y2_window, x_max_true, x
 
 def return_coordinates(y_pred):
     global _epsilon
-    xpred = K.sigmoid(y_pred[:, :, :, 1])
-    xpred = tf.clip_by_value(t=xpred, clip_value_min = _epsilon, clip_value_max = 1 - _epsilon)
+    xpred = y_pred[:, :, :, 1]
+    xpred = tf.clip_by_value(t=xpred, clip_value_min = 0 + _epsilon, clip_value_max = 1 - _epsilon)
     xpred = K.cast(xpred, 'float32')
     xpred = xpred * 64 + np.arange(0, 608, 32).reshape(19, 1)
-    ypred = K.sigmoid(y_pred[:, :, :, 2])
-    ypred = tf.clip_by_value(t=ypred, clip_value_min = _epsilon, clip_value_max = 1 - _epsilon)
+    ypred = y_pred[:, :, :, 2]
+    ypred = tf.clip_by_value(t=ypred, clip_value_min = 0 + _epsilon, clip_value_max = 1 - _epsilon)
     ypred = K.cast(ypred, 'float32')
     ypred = ypred * 64 + np.arange(0, 417, 32).reshape(1, 14)
-    wpred = K.sigmoid(y_pred[:, :, :, 3]) * 64 * 10
+    wpred = y_pred[:, :, :, 3] * 64 * 10
     wpred = K.clip(x=wpred, max_value=640, min_value=50)
-    hpred = K.sigmoid(y_pred[:, :, :, 4]) * 64 * 10
+    hpred = y_pred[:, :, :, 4] * 64 * 10
     hpred = K.clip(x=hpred, max_value=480, min_value=50)
     return [xpred, ypred, wpred, hpred]
 
 def Loss_v2(y_true, y_pred):
     Pc_pred = y_pred[:, :, :, 0]
-    Pc_pred = K.sigmoid(x=Pc_pred)
+    Pc_pred = K.cast(Pc_pred, 'float32')
     global _epsilon
     Pc_pred = tf.clip_by_value(t=Pc_pred, clip_value_min = _epsilon, clip_value_max = 1 - _epsilon)
-    Pc_pred = K.cast(Pc_pred, 'float32')
     xpred, ypred, wpred, hpred = return_coordinates(y_pred)
     x1_pred, y1_pred, x2_pred, y2_pred = transform_to_coordinate(xpred, ypred, wpred, hpred)
 
@@ -149,17 +115,16 @@ def Loss_v2(y_true, y_pred):
                                   y_max_true, y_min_true)
     mat = K.cast(matching, 'float32')
 
-
-    C_Class_Array = softmax(x = C_Class_Array, axis= -1)
     C_Class_Array = tf.clip_by_value(t=C_Class_Array, clip_value_min = _epsilon, clip_value_max = 1 - _epsilon)
     Classification_loss = categorical_crossentropy(y_true= C_index_true, y_pred= C_Class_Array)
 
     Classification_loss = K.reshape(x=Classification_loss, shape=(-1, 19, 14, 1))
 
-    Classification_loss  = (1 - C_Class_Array) * C_index_true * Classification_loss *weight_Classification_loss
+    Classification_loss  = (1 - C_Class_Array) * C_index_true * Classification_loss * weight_Classification_loss
 
     Localization_loss = weight_Localization_loss * mat * (K.square(x1_pred - x_min_true) + K.square(x2_pred - x_max_true) + K.square(
         y1_pred - y_min_true) + K.square(y2_pred - y_max_true))
+
     Object_loss = -(1 - Pc_pred) * K.log(Pc_pred) * mat - (1 - mat) * K.log(1-Pc_pred) * Pc_pred
     Object_loss = Object_loss * weight_Object_loss
 
@@ -171,7 +136,6 @@ def Loss_v2(y_true, y_pred):
 
 def Loss_v3(y_true, y_pred):
     Pc_pred = y_pred[:, :, :, 0]
-    Pc_pred = K.sigmoid(x=Pc_pred)
     Pc_pred = K.cast(Pc_pred, 'float32')
 
     xpred, ypred, wpred, hpred = return_coordinates(y_pred)
@@ -198,10 +162,9 @@ def Loss_v3(y_true, y_pred):
     matching = Checking_if_object(x1_window, y1_window, x2_window, y2_window, x_max_true, x_min_true,
                                   y_max_true, y_min_true)
     mat = K.cast(matching, 'float32')
-    
+   
     global _epsilon
 
-    C_Class_Array = softmax(x = C_Class_Array, axis= -1)
     C_Class_Array = tf.clip_by_value(t=C_Class_Array, clip_value_min = _epsilon, clip_value_max = 1 - _epsilon)
     
     Classification_loss = categorical_crossentropy(y_true= C_index_true, y_pred= C_Class_Array) * weight_Classification_loss
@@ -215,3 +178,32 @@ def Loss_v3(y_true, y_pred):
     Totalloss = K.mean(x=Total_loss, axis=-1)
 
     return Totalloss
+
+class DecayByEpoch(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, log=[]):
+        global Hdecay
+        new_lr = initial_lr * 1.0 / (1.0 + Hdecay * epoch)
+        if initial_lr / new_lr > lr_minimum_rate:
+            lr = self.model.optimizer.lr
+        else:
+            K.set_value(self.model.optimizer.lr, new_lr)
+            lr = self.model.optimizer.lr
+        print(K.eval(lr))
+
+class lr_minimum(keras.callbacks.Callback):
+    def on_batch_end(self, batch, log=[]):
+        global Hdecay, _epoch, _batch_size
+        iterations = batch + _epoch * 1000.0 / _batch_size
+        print('iterations:', iterations)
+
+        new_lr = initial_lr * 1.0 / (1.0 + Hdecay * iterations)
+        print('The New_lr:', new_lr)
+        if initial_lr / new_lr > lr_minimum_rate:
+            K.set_value(self.model.optimizer.lr, initial_lr/lr_minimum_rate)
+        else:
+            K.set_value(self.model.optimizer.lr, new_lr)
+    def on_epoch_end(self, epoch, log=[]):
+        lr = self.model.optimizer.lr
+        global _epoch
+        _epoch += 1
+        print('Each epoch, the lr is', K.eval(lr))
---
+++ starter_predict.py
@@ -0,0 +1,191 @@
+import keras.backend as K
+from keras.models import load_model
+import xml.etree.ElementTree as ET
+from keras.preprocessing.image import load_img, img_to_array, array_to_img
+from keras.optimizers import Adam
+import numpy as np
+import tensorflow as tf
+from datetime import datetime
+import os
+from settings import setting
+import cv2
+from keras.utils.np_utils import to_categorical
+
+
+img_count = 0
+
+############################################
+if setting["architecture"] == "Yolo_V1":
+    import Util_V1 as U
+elif setting["architecture"] == "Yolo_V2":
+    import Util_V2 as U
+if setting['model'] == "DecayByBatch":
+    optimizer = Adam(lr=setting["lr"])
+elif setting['model'] == "DecayByEpoch":
+    optimizer = Adam(lr=setting["lr"])
+if setting['loss'] == 'Loss_v2':
+    lossFunction = U.Loss_v2
+elif setting['loss'] == 'Loss_v3':
+    lossFunction = U.Loss_v3
+#############################################
+def draw(img, boxes, scores, classes):
+    """Draw the boxes on the image.
+
+    # Argument:
+        image: original image.
+        boxes: ndarray, boxes of objects.
+        classes: ndarray, classes of objects.
+        scores: ndarray, scores of objects.
+        all_classes: all classes name.
+    """
+    global img_count
+
+    num_indices = tf.image.non_max_suppression(boxes, scores, max_output_size=3, iou_threshold=0.8)
+    boxes = tf.gather(params=boxes, indices=num_indices)
+    scores = tf.gather(params=scores, indices=num_indices)
+    classes = tf.gather(params=classes, indices=num_indices)
+
+    boxes = K.eval(boxes)
+    scores = K.eval(scores)
+    classes = K.eval(classes)
+    for box, score, cl in zip(boxes, scores, classes):
+        x1, y1, x2, y2 = box
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        cv2.putText(img, 'C'+str(cl), (int((x2+x1)/2.0), int((y1+y2)/2.0) - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 1, cv2.LINE_AA)
+        print('cl:', cl)
+    img = img.transpose()
+    img = array_to_img(img, data_format='channels_first')
+    img.show()
+    img.save('result'+str(img_count)+'.png')
+    img_count += 1
+
+def Matching(x1_pred, y1_pred, x2_pred, y2_pred, x_max_true, x_min_true, y_max_true, y_min_true):
+    intersect_x1 = K.maximum(x1_pred, x_min_true)
+    intersect_y1 = K.maximum(y1_pred, y_min_true)
+    intersect_x2 = K.minimum(x2_pred, x_max_true)
+    intersect_y2 = K.minimum(y2_pred, y_max_true)
+
+    area_1 = (x2_pred - x1_pred) * (y2_pred - y1_pred)
+    area_2 = (x_max_true - x_min_true) * (y_max_true - y_min_true)
+
+    intersect_area = (intersect_x2 - intersect_x1) * (intersect_y2 - intersect_y1)
+
+    IOU = intersect_area / (area_1 + area_2 - intersect_area)
+    matching = K.greater_equal(IOU, 0.7)
+    return matching
+
+def LOAD_IMAGE(path = 'C:/Users/ZJZ20\Downloads/DeepLearnData/img_0.jpg'):
+    image1 = load_img(path)
+    train_example = img_to_array(image1, data_format='channels_first')
+    img = array_to_img(train_example, data_format='channels_first')
+#    img.show()
+    train_example = train_example.transpose()
+    return train_example
+
+
+def readXML(f='C:/Users/ZJZ20\Downloads/DeepLearnData/img_0.xml'):
+    mapped = {}
+    tree = ET.parse(f)
+    root = tree.getroot()
+    for elem in root:
+        if len(elem) == 0:
+            mapped[elem.tag] = elem.text
+        for subelem in elem:
+            if len(subelem) == 0:
+                mapped[subelem.tag] = subelem.text
+            for sub2elem in subelem:
+                if len(sub2elem) == 0:
+                    mapped[sub2elem.tag] = sub2elem.text
+    xmax = int(mapped['xmax'])
+    xmin = int(mapped['xmin'])
+    ymax = int(mapped['ymax'])
+    ymin = int(mapped['ymin'])
+    C_index = int(mapped['name'][1])
+    yTrue = np.zeros((19, 14, 7), dtype='float32')
+    yTrue[:, :] = [xmax, xmin, ymax, ymin, 0, 0, C_index]
+    return yTrue
+
+
+def load_DATA(srcDir):
+    fileCount = len([name for name in os.listdir(srcDir) if name.endswith(".xml")])
+    train_data = np.empty(shape=[fileCount, 640, 480, 3], dtype='float32')
+    train_label = np.empty(shape=[fileCount, 19, 14, 7], dtype='float32')
+
+    count = 0
+    for filename in os.listdir(srcDir):
+        if not filename.endswith(".xml"): continue
+        count += 1
+        xmlFile = srcDir + "/" + filename
+        print(xmlFile)
+        imgFile = xmlFile.replace(".xml", ".jpg")
+        print(imgFile)
+        train_data[count - 1] = LOAD_IMAGE(imgFile)
+        train_label[count - 1] = readXML(xmlFile)
+    train_label[:, :, :, 4] = np.arange(0, 19, 1).reshape(19, 1)
+    train_label[:, :, :, 5] = np.arange(0, 14, 1).reshape(1, 14)
+    return [train_data, train_label]
+
+def Testing_Performance_location(y_true, y_pred):
+    boxes, classes, scores = U.generating_consequences(y_pred)
+    x1, y1, x2, y2 = U.transform_to_coordinate(boxes[:, :, 0], boxes[:, :, 1], boxes[:, :, 2], boxes[:, :, 3])
+    x1 = K.reshape(x=x1, shape=(setting['batch_size'], -1, 1))
+    x2 = K.reshape(x=x2, shape=(setting['batch_size'], -1, 1))
+    y1 = K.reshape(x=y1, shape=(setting['batch_size'], -1, 1))
+    y2 = K.reshape(x=y2, shape=(setting['batch_size'], -1, 1))
+    Boxes = K.concatenate([x1, y1, x2, y2])
+    x1_pred = Boxes[:, :, 0]
+    y1_pred = Boxes[:, :, 1]
+    x2_pred = Boxes[:, :, 2]
+    y2_pred = Boxes[:, :, 3]
+
+    x_max_true = y_true[:, 0, 0, 0]
+    x_min_true = y_true[:, 0, 0, 1]
+    y_max_true = y_true[:, 0, 0, 2]
+    y_min_true = y_true[:, 0, 0, 3]
+    x_max_true = K.reshape(x_max_true, (setting['batch_size'], 1))
+    x_min_true = K.reshape(x_min_true, (setting['batch_size'], 1))
+    y_max_true = K.reshape(y_max_true, (setting['batch_size'], 1))
+    y_min_true = K.reshape(y_min_true, (setting['batch_size'], 1))
+    IOU = Matching(x1_pred=x1_pred, y1_pred=y1_pred, x2_pred=x2_pred, y2_pred=y2_pred, x_max_true=x_max_true,
+                   x_min_true=x_min_true, y_max_true=y_max_true, y_min_true=y_min_true)
+    IOU = K.cast(IOU, 'float32')
+    mat = K.max(IOU, axis=-1)
+    return K.cast(tf.count_nonzero(input_tensor=mat), 'float32') / setting['batch_size']
+
+def Testing_Performance_classification(y_true, y_pred):
+    boxes, classes, scores = U.generating_consequences(y_pred)
+    groundtrue_class = y_true[:, 0, 0, 6]
+    groundtrue_class = K.cast(groundtrue_class, 'int64')
+    groundtrue_class = K.reshape(groundtrue_class, (setting['batch_size'], 1))
+    classification_loss = K.equal(classes, groundtrue_class)
+    classification_loss = K.cast(x=classification_loss, dtype='float32')
+    class_match = K.max(classification_loss, axis=-1)
+    return K.cast(tf.count_nonzero(input_tensor=class_match), 'float32') / setting['batch_size']
+
+if __name__ == '__main__':
+    model = load_model('weight-setting5.h5', custom_objects={setting["loss"]: lossFunction})
+    srcDir = "D:/Dataset/YoLo/4"
+    print("+++ run: " + srcDir + " " + str(datetime.now()) + "+++")
+    train_data, train_label = load_DATA(srcDir=srcDir)
+    one_hot_encoding = to_categorical(y=train_label[:, :, :, 6], num_classes=10)
+    train_label = np.concatenate((train_label, one_hot_encoding), axis = -1)
+    model.compile(optimizer=optimizer, loss=lossFunction, metrics=[Testing_Performance_location, Testing_Performance_classification])
+    loss_and_metrics = model.evaluate(x=train_data, y=train_label, batch_size=setting['batch_size'])
+    print('location_loss', loss_and_metrics[1])
+    print('classification_loss', loss_and_metrics[2])
+
+    results = model.predict(x=train_data, batch_size=setting['batch_size'])
+    Boxes, Classes, Scores = U.generating_consequences(results)
+    x1, y1, x2, y2 = U.transform_to_coordinate(Boxes[:, :, 0], Boxes[:, :, 1], Boxes[:, :, 2], Boxes[:, :, 3])
+    x1 = K.reshape(x=x1, shape=(results.shape[0], -1, 1))
+    x2 = K.reshape(x=x2, shape=(results.shape[0], -1, 1))
+    y1 = K.reshape(x=y1, shape=(results.shape[0], -1, 1))
+    y2 = K.reshape(x=y2, shape=(results.shape[0], -1, 1))
+    Boxes = K.concatenate([x1, y1, x2, y2])
+    for i in range(setting['batch_size']):
+        draw(train_data[i], boxes=Boxes[i], scores=Scores[i], classes=Classes[i])
+
+
+
+
+
